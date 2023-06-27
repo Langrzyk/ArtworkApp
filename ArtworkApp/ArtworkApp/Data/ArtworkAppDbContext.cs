5be4e001b45a220fe7b54c5e899af6a628b52af9
Betinka <betina.langrzyk@gmail.com>
2023-06-27 18:26:08 +0200

    Add SqlServer connections

--- ArtworkApp/ArtworkApp/Data/ArtworkAppDbContext.cs
+++ ArtworkApp/ArtworkApp/Data/ArtworkAppDbContext.cs
@@ -2,17 +2,24 @@
 
 using ArtworkApp.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class ArtworkAppDbContext : DbContext
 {
+    private readonly string _connectionString = @"Server=(localdb)\mssqllocaldb;Database=ArtworkAppDB";
+
     public DbSet<Painting> Paintings => Set<Painting>();
 
     public DbSet<Sculpture> Sculptures => Set<Sculpture>();
+    public ArtworkAppDbContext()
+    {
+    }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
-        optionsBuilder.UseInMemoryDatabase("StorageAppDb");
+        optionsBuilder.UseSqlServer(_connectionString);
+        //optionsBuilder.UseInMemoryDatabase("StorageAppDb");
     }
 
 }
--- ArtworkApp/ArtworkApp/Entities/EntityBase.cs
+++ ArtworkApp/ArtworkApp/Entities/EntityBase.cs
@@ -1,7 +1,12 @@
-﻿namespace ArtworkApp.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArtworkApp.Entities;
 
 public class EntityBase : IEntity
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     public string? Title { get; set; }
